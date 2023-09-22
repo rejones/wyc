bbf745f9cea6daf6f412912d00d38b4c36e0997d
Richard Jones <r.e.jones@kent.ac.uk>
2023-09-22 13:53:04 +0100

    Use rsync

--- web/update.sh
+++ web/update.sh
@@ -4,5 +4,5 @@ SERVER=myrtle.kent.ac.uk
 TARGET_DIR=webpages/wyc/web
 FILES="banner.png instructions.html script.js styles.css sv-modal.css sv-modal.js sv-modal.scss xl2cal.html xlsx.mini.min.js"
 
-echo scp $FILES $SERVER:$TARGET_DIR
-scp $FILES $SERVER:$TARGET_DIR
+echo rsync -avuh -e ssh $FILES $SERVER:$TARGET_DIR
+rsync -avuh -e ssh $FILES $SERVER:$TARGET_DIR
