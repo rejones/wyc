64057a655c90c146246c8d26f0441958e651ab55
Richard Jones <r.e.jones@kent.ac.uk>
2023-10-06 10:22:30 +0100

    License

--- web/script.js
+++ web/script.js
@@ -1,11 +1,44 @@
 // script.js
 
 /**
- * Convert WYC racing schedule into .ics format that can be loaded into a calendar.
+ * The xl2cal system converts an event schedule (typically, the Whitstable
+ * Yacht Club) racing schedule from an Excel spreadsheet to .ics format 
+ * that can be loaded into a calendar.
+ *
+ * Details of usage and the requirements of the spreadsheet can be found in
+ * instructions.html.
+ * In general, xl2cal attempts to be as lenient as possible with spreadsheet
+ * data such as dates and times, and permits a variety of formats for these.
+ * See comments below about peculiarities of Excel formats, etc.
+ *
  * @author Richard Jones
  * @copyright Richard Jones, 2023
  * https://github.com/rejones/wyc/web
  */
+
+/*
+Copyright 2023 Richard Jones
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “Software”), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*
+*/
 
 const dayNames = ['Sun', 'Mon', 'Tues', 'Weds', 'Thurs', 'Fri', 'Sat'];
 const monthNames = ['January','February','March','April','May','June','July','August',
